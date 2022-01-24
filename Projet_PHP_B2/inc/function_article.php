11231974cd792774b792f1b40dad83aabec9c6a9
Andréas CASTELLO <andreas.castello06@gmail.com>
2022-01-24 17:53:50 +0100

    fin partie andréas
    voila c'est la fin

--- Projet_PHP_B2/inc/function_article.php
+++ Projet_PHP_B2/inc/function_article.php
@@ -3,7 +3,7 @@
 $bdd = new PDO($dsn, $user, $password);
 
 $bddArt = $bdd->prepare('SELECT * FROM article');
-$nameUser = $bdd->query('SELECT nom_i FROM article inner join inscrit on article.user_a = inscrit.id_i');
+$nameUser = $bdd->query('SELECT nom_i FROM inscrit order by id_i');
 
 $execute = $bddArt->execute();
 $articles = $bddArt->fetchAll();
--- Projet_PHP_B2/inc/function_article_edit.php
+++ Projet_PHP_B2/inc/function_article_edit.php
@@ -2,3 +2,26 @@
 
 $pdo = new PDO($dsn, $user, $password);
 
+if (isset($_POST['edit'])) {
+
+    $destination = "./assets/IMG/article/";
+    $titre = $_POST['title'];
+    $contenu = $_POST['message'];
+    $image = $_POST['file'];
+        $pdoStat = $pdo->prepare('UPDATE `article` (`id_a`, `titre_a`, `contenu_a`, `image_a`, `date_a`, `user_a`) SET (NULL, :titre_a, :contenu_a, :image_a, CURRENT_DATE, 2)');
+    
+        $pdoStat->bindValue(':titre_a', $titre, PDO::PARAM_STR);
+        $pdoStat->bindValue(':contenu_a', $contenu, PDO::PARAM_STR);
+        $pdoStat->bindValue(':image_a', $destination . $image, PDO::PARAM_STR);
+    
+        $exe = $pdoStat->execute();
+    
+        if ($exe) {
+            echo "article modifié";
+        }
+        else{
+            echo "NON";
+        }
+    }
+    
+    
